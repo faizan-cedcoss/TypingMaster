6973141b61be09c309fc8a7a021e9d121955d0bf
faizan7275 <74813450+faizan7275@users.noreply.github.com>
2020-12-04 09:22:26 +0530

    Update typing.js
    ​
    2
    var a,c,b,d;
    3
    function start()
    4
    {       
    5
            a=document.querySelector('#ques').value;        
    6
            var s=0;
    7
            var m=0;
    8
            var h=0;
    9
            b=1;
    10
            d=0;
    11
            for (var i = 0; i < a.length; i++) {
    12
                    if(a.charAt(i)==' ')
    13
                            b++;
    14
            }
    15
            var z=setInterval(timer,1000);
    16
            function timer()
    17
            {       d++;
    18
                    c=document.getElementById('ans').value;
    19
                    if (s<10)
    20
                            document.getElementById('sec').innerHTML="0"+s;
    21
                    else
    22
                            document.getElementById('sec').innerHTML=s;
    23
                    if (m<10)
    24
                            document.getElementById('min').innerHTML="0"+m;
    25
                    else
    26
                            document.getElementById('min').innerHTML=m;
    27
                    document.getElementById('hour').innerHTML="0"+h;
    28
                    s++;
    29
                    if (s==60){
    30
                            m++;
    31
                            s-=60;
    32
                    }
    33
                    if (m==60){
    34
                            h++;
    35
                            m-=60;
    36
                    }
    37
                    if(c==a)
    38
                    {
    39
                            clearInterval(z);
    40
                            var speed=parseInt((b/d)*60);
    41
                            document.getElementById('wpm').innerHTML=speed;
    42
                    }
    43
            }               
    44
            }
    45
            function reset()

--- typing.js
+++ typing.js
@@ -1,21 +1,35 @@
-
 var a,c,b,d;
+var j1=1;
+function start1()
+{
+	if(j1==1)
+	{
+		start();
+	}
+	j1++;
+}
 function start()
 {	
-	a=document.querySelector('#ques').value;	
+	a=document.querySelector('#ques').innerText;	
 	var s=0;
 	var m=0;
 	var h=0;
 	b=1;
 	d=0;
-	for (var i = 0; i < a.length; i++) {
+	for (var i = 0; i < a.length; i++) 
+	{
 		if(a.charAt(i)==' ')
 			b++;
 	}
-	var z=setInterval(timer,1000);
+	var z=setInterval(timer,10);
 	function timer()
-	{	d++;
+	{	
 		c=document.getElementById('ans').value;
+		var e=a.substring(0,c.length);
+		if(c==e)
+			document.getElementById('ans').style.borderColor="blue";
+		else
+			document.getElementById('ans').style.borderColor="red";
 		if (s<10)
 			document.getElementById('sec').innerHTML="0"+s;
 		else
@@ -26,11 +40,13 @@ function start()
 			document.getElementById('min').innerHTML=m;
 		document.getElementById('hour').innerHTML="0"+h;
 		s++;
-		if (s==60){
+		if (s==100){
+			d++;
 			m++;
-			s-=60;
+			s-=100;
 		}
 		if (m==60){
+
 			h++;
 			m-=60;
 		}
@@ -44,6 +60,5 @@ function start()
 	}
 	function reset()
 	{
-		window.location.href="index.html";
+		location.reload();
 	}
-
