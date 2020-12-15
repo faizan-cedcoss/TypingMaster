696e3de9e98979c6b5f215821474527143858f1b
faizan7275 <74813450+faizan7275@users.noreply.github.com>
2020-12-15 13:47:12 +0530

    Update typing.js

--- typing.js
+++ typing.js
@@ -1,64 +1,65 @@
-var a,c,b,d;
-var j1=1;
-function start1()
-{
-	if(j1==1)
-	{
+var orignal, typed, totalWords, totalSec;
+var j1 = 0;
+totalWords = 1;
+totalSec = 0;
+orignal = document.querySelector('#ques').innerText;
+var len = orignal.length;
+var wrong = len;
+for (var i = 0; i < len; i++) {
+	if (orignal.charAt(i) == ' ')
+		totalWords++;
+}
+function start1() {
+	typed = document.getElementById('ans').value;
+	if (j1 == 0) {
 		start();
+	}
+	typed = document.getElementById('ans').value;
+	var sub = orignal.substring(0, typed.length);
+	if (typed == sub) {
+		document.getElementById('ans').style.borderColor = "blue";
+	}
+	else {
+		document.getElementById('ans').style.borderColor = "red";
+		wrong--;
 	}
 	j1++;
 }
-function start()
-{	
-	a=document.querySelector('#ques').innerText;	
-	var s=0;
-	var m=0;
-	var h=0;
-	b=1;
-	d=0;
-	for (var i = 0; i < a.length; i++) 
-	{
-		if(a.charAt(i)==' ')
-			b++;
-	}
-	var z=setInterval(timer,10);
-	function timer()
-	{	
-		c=document.getElementById('ans').value;
-		var e=a.substring(0,c.length);
-		if(c==e)
-			document.getElementById('ans').style.borderColor="blue";
+function start() {
+	var msec = 0;
+	var sec = 0;
+	var min = 0;
+	totalSec = 0;
+	var z = setInterval(timer, 10);
+	function timer() {
+		if (msec < 10)
+			document.getElementById('sec').innerHTML = "0" + msec;
 		else
-			document.getElementById('ans').style.borderColor="red";
-		if (s<10)
-			document.getElementById('sec').innerHTML="0"+s;
+			document.getElementById('sec').innerHTML = msec;
+		if (sec < 10)
+			document.getElementById('min').innerHTML = "0" + sec;
 		else
-			document.getElementById('sec').innerHTML=s;
-		if (m<10)
-			document.getElementById('min').innerHTML="0"+m;
-		else
-			document.getElementById('min').innerHTML=m;
-		document.getElementById('hour').innerHTML="0"+h;
-		s++;
-		if (s==100){
-			d++;
-			m++;
-			s-=100;
+			document.getElementById('min').innerHTML = sec;
+		document.getElementById('hour').innerHTML = "0" + min;
+		msec++;
+		if (msec == 100) {
+			totalSec++;
+			sec++;
+			msec -= 100;
 		}
-		if (m==60){
-
-			h++;
-			m-=60;
+		if (sec == 60) {
+			min++;
+			sec -= 60;
 		}
-		if(c==a)
-		{
+		if (typed == orignal) {
 			clearInterval(z);
-			var speed=parseInt((b/d)*60);
-			document.getElementById('wpm').innerHTML=speed;
+			var speed = parseInt((totalWords / totalSec) * 60);
+			document.getElementById('wpm').innerHTML = speed;
+			var acc = (wrong / len) * 100;
+			document.getElementById('accuracy').innerHTML = acc.toFixed(2);
 		}
-	}		
 	}
-	function reset()
-	{
-		location.reload();
-	}
+}
+function reset() {
+	location.reload();
+}
